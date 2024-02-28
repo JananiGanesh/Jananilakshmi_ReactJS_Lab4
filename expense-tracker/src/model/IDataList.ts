a900a4b3582ec0de2b1e841f74facedc9a9a5ec2
Janani Ganesh <g.jananilakshmimca@gmail.com>
2024-02-28 07:58:28 +0530

    Initial commit

---
+++ expense-tracker/src/model/IDataList.ts
@@ -0,0 +1,11 @@
+interface IDataList{
+    
+        payeeName:string,
+        product: string,
+        price: number,
+        setDate: string,
+        id: number
+        
+}
+
+export default IDataList;
